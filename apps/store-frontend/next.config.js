6c8a702acd911a7faa778144654ce0c978a71bce
Dan Barr <dan@dbarr.com>
2024-05-13 16:40:25 -0400

    Upgrade app dependencies

--- apps/store-frontend/next.config.js
+++ apps/store-frontend/next.config.js
@@ -1,14 +1,12 @@
-/** 
- * @type {import('next').NextConfig} 
+/**
+ * @type {import('next').NextConfig}
  * */
 
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
+  images: {
+    unoptimized: true,
   },
-}
+};
 
 module.exports = nextConfig;
